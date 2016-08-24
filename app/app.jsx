d4b15115788b40a4bd6820824d415ae3163a5033
Arthur Christoph <achristoph@gmail.com>
2016-08-24 01:50:35 -0500

    .

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
-import NavBar from './navbar';
-import Content from './content';
-import '../css/normalize.css';
-import '../css/skeleton.css';
-import '../css/main.css';
+import NavBar from './components/navbar';
+import Content from './components/content';
+import './css/normalize.css';
+import './css/skeleton.css';
+import './css/main.css';
 
 class App extends Component {
   render() {
