9799fb9f5604a85ca0b9f0a8eeda5ee9c6a6a000
Arthur Christoph <achristoph@gmail.com>
2016-10-26 16:22:58 -0500

    .

--- app/contact.jsx
+++ app/contact.jsx
@@ -1,5 +1,5 @@
 import NavBar from './navbar';
 import React from 'react';
 import { Router, Route, Link, hashHistory } from 'react-router';
-const Contact = (props) => <div><h1>Contact {props.params.id}</h1></div>;
+const Contact = (props) => <div><h1>Contact {props.params.id} {props.location.query.message}</h1></div>;
 export default Contact;
--- app/navbar.jsx
+++ app/navbar.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import { Link } from 'react-router';
 
 class NavBar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <div className="logo"/>
+        <div className="logo" />
         <div className="container">
           <ul className="navbar-list">
-            <li className="navbar-item"><Link to="/" activeStyle={{color:'green'}} className="navbar-link">Home</Link></li>
-            <li className="navbar-item"><Link to="/about" activeStyle={{color:'green'}} className="navbar-link">About</Link></li>
-            <li className="navbar-item"><Link to="/about/contact" activeStyle={{color:'green'}} className="navbar-link">Contact</Link></li>
+            <li className="navbar-item"><Link to="/" activeStyle={{ color: 'green' }} className="navbar-link">Home</Link></li>
+            <li className="navbar-item"><Link to="/about" activeStyle={{ color: 'green' }} className="navbar-link">About</Link></li>
+            <li className="navbar-item"><Link to={{ pathname: '/about/contact', query: { message: 'Yo' } }} activeStyle={{ color: 'green' }} className="navbar-link">Contact</Link></li>
           </ul>
         </div>
       </nav>
